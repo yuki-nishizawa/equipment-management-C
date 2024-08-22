0854fb870b88d1b2d4d7dfca689dc98eddd24367
yuki-nishizawa <yuki-nishizawa@tokyu-hl.jp>
2024-08-22 17:13:07 +0900

    備品発注機能追加・テンプレート微修正
    動作確認済/発注履歴ページを作成、伴ってヘッダーリンクを修正しました。また、テーブルタグを使用しているページについて一旦体裁を統一させました。

--- config/urls.py
+++ config/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.conf.urls.static import static
 from django.conf import settings
 from django.urls import path,include
-from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('equipment/', include('equipment.urls')),#equipmentのurls.pyへ接続
     path('', include('users.urls')),              #usersアプリのurls.pyへ接続
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('order_history/', include('order.urls')),  # orderアプリのurls.pyへ接続
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)#画像ファイルをどこに格納するか→場所はsettings.pyで指定
--- order/views.py
+++ order/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Order
 
-# Create your views here.
+# 発注履歴表示機能
+class OrderListView(LoginRequiredMixin, ListView):
+    template_name = 'order/history.html'
+    model = Order
+    context_object_name = 'orders'
+    #paginate_by = 5  # 1ページに表示する発注数：ページ送り機能を付ける際に使用
+
+    def get_queryset(self):
+        # すべての注文を取得し、発注日の降順に並べ替える
+        queryset = Order.objects.order_by('-order_date')
+        
+        # GETパラメータからフィルタリングのステータスを取得
+        #status_filter = self.request.GET.get('status_filter')
+        
+        # フィルタリングが選択されている場合は、クエリセットをフィルタリング：ページ送り機能を付ける際に使用
+        #if status_filter:
+        #    queryset = queryset.filter(approval_status=status_filter)
+        
+        return queryset
